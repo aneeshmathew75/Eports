478d45e74e2383da922d5e957ad9659edd91324e
Mohammad Jonaid Ansari <ansari.jonaid@gmail.com>
2021-11-22 20:30:19 +0530

    filter card changes

---
+++ Home page/assets/js/data.js
@@ -0,0 +1,47 @@
+const eventData = [
+    {
+        eventId: '1',
+        name: 'aaaa',
+        hostedBy: '',
+        logo: '',
+        eventDate: '',
+        genre: '',
+        eventImage: '../../img/03.jpg'
+    },
+    {
+        eventId: '2',
+        name: 'bbbb',
+        hostedBy: '',
+        logo: '',
+        eventDate: '',
+        genre: '',
+        eventImage: 'fifa.jpg'
+    },
+    {
+        eventId: '3',
+        name: 'cccc',
+        hostedBy: '',
+        logo: '',
+        eventDate: '',
+        genre: '',
+        eventImage: 'fifa.jpg'
+    },
+    {
+        eventId: '4',
+        name: 'dddd',
+        hostedBy: '',
+        logo: '',
+        eventDate: '',
+        genre: '',
+        eventImage: 'fifa.jpg'
+    },
+    {
+        eventId: '5',
+        name: 'eeee',
+        hostedBy: '',
+        logo: '',
+        eventDate: '',
+        genre: '',
+        eventImage: 'fifa.jpg'
+    },
+];
